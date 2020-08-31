1950fbd8706255cd6d89aaf1214514c78daae8cd
Dhairyasheel <sutardhiraj98@gmail.com>
2020-08-31 20:26:54 +0530

    updated main file

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask
-from flask import request
-from google.cloud import pubsub
+from flask import Flask, request, jsonify
+import base64
 from google.cloud import logging
 
 app = Flask(__name__)
@@ -10,9 +9,17 @@ logger = logging_client.logger(log_name)
 
 @app.route("/")
 def index():
-    print(request.json)
-    print(request.form)
-    return "Hello world from kubernetes!"
+
+    # Receive the input
+    request_json = request.get_json()
+    message = base64.b64decode(request_json['message']['data']).decode("utf-8")
+
+    # Logging text
+    logger.log_text(message)
+    print(message)
+
+    # Return response in json format
+    return jsonify({"message": message}, 200);
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
