1d28e38cfaec192c22afae145ab8ac5adcc0a5e1
Dhairyasheel <sutardhiraj98@gmail.com>
2020-08-31 21:17:14 +0530

    using pull subscription

--- app.py
+++ app.py
@@ -1,25 +1,36 @@
 from flask import Flask, request, jsonify
+from concurrent.futures import TimeoutError
 import base64
 from google.cloud import logging
+from google.cloud import pubsub
 
 app = Flask(__name__)
 logging_client = logging.Client()
 log_name = 'gke-application-logs'
 logger = logging_client.logger(log_name)
+PROJECT_ID = 'training-freshers'
+TOPIC = 'topic-gke-cluster'
+SUB = 'subscription-gke-topic'
+subscriber = pubsub.SubscriberClient()
+
+def callback(message):
+    logger.log_text(message.data.decode("utf-8"))
+    print(message.data)
+    message.ack()
 
 @app.route("/")
 def index():
 
-    # Receive the input
-    request_json = request.get_json()
-    message = base64.b64decode(request_json['message']['data']).decode("utf-8")
-
-    # Logging text
-    logger.log_text(message)
-    print(message)
+    # Receive the input    
+    subscription_path = subscriber.subscription_path(PROJECT_ID, SUB)
+    future = subscriber.subscribe(subscription_path, callback)
+    try:
+        future.result(timeout=3.0)
+    except:
+        future.cancel()
 
     # Return response in json format
-    return jsonify({"message": message}, 200);
+    return jsonify({"message": "Message read successfully"}, 200);
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
